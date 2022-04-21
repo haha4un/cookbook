5df5ec038f5f48be7310228e5ef3545c28064904
haha4un <savelijstulev3872@gmail.com>
2022-04-21 16:44:31 +0500

    few updates //

--- app/src/main/java/com/example/cookbook/MainActivity.kt
+++ app/src/main/java/com/example/cookbook/MainActivity.kt
@@ -1,81 +1,83 @@
 package com.example.cookbook
 
+import android.content.res.Resources
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toDrawable
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    var test: TextView ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var test: TextView = findViewById(R.id.test)
-        var scrool: LinearLayout = findViewById(R.id.scrooler)
-        var child: TextView = TextView(this)
-        var image: ImageView = ImageView(this)
+         test = findViewById(R.id.test)
+         var scrool: LinearLayout = findViewById(R.id.scrooler)
+         var child: ImageView = ImageView(this)
 
-
-        image.setImageURI(Uri.parse("https://klike.net/uploads/posts/2019-06/1559545617_2.jpg"))
-
-        var str = getFilesDir().getPath() + "cooks.sql"
+         var str = getFilesDir().getPath() + "cooks.sql"
 
         var base: SQLiteDatabase = baseContext.openOrCreateDatabase("cook.db", MODE_PRIVATE, null)
+        base.execSQL("CREATE TABLE IF NOT EXISTS cooks (name, what)")
+        base.execSQL("INSERT INTO cooks VALUES ('Кулябяка', 'Куипи')")
+        base.execSQL("INSERT INTO cooks VALUES ('Кулябяка', 'Куипи')")
+        base.execSQL("INSERT INTO cooks VALUES ('Кулябяка', 'Куипи')")
 
-        child.textSize = 35f
-        child.text = BaseReader(base)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
-        add(child.text.toString(), scrool)
+
+        scrool.addView(child)
+        add("f", scrool)
+        add("f", scrool)
+        add("f", scrool)
+        add("f", scrool)
+        add("f", scrool)
+        add("f", scrool)
+        add("f", scrool)
+        add("f", scrool)
+        add("f", scrool)
+        add("f", scrool)
+        add("f", scrool)
+
+        str = BaseReader(base)
     }
 
     fun BaseReader(db: SQLiteDatabase) : String
     {
-        var cursor: Cursor = db.rawQuery("SELECT * FROM cook", null)
+        var cursor: Cursor = db.rawQuery("SELECT * FROM cooks", null)
 
         var str: String = "БЛЮДО: "
-        if(cursor.moveToFirst())
+        while(cursor.moveToNext())
         {
              str += cursor.getString(0)
             str += " Рецепт: "
              str+= cursor.getString(1)
         }
+
+        cursor.close()
         return str;
     }
 
-    fun add(str: String, scrool: LinearLayout)
+    fun add(res: String, scrool: LinearLayout, db: SQLiteDatabase)
     {
-        var noneContent: ImageView = ImageView(this)
-//        noneContent.text = str
+        var noneContent: ImageButton = ImageButton(this)
+        noneContent.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.img))
         scrool.addView(noneContent)
+
+        noneContent.setOnClickListener()
+        {
+            BaseReader(db)
+        }
     }
 }
